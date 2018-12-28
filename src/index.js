390c0be1d21c00baae378818d00060b0a45820c3
Humminggoo <sashap1997@gmail.com>
2018-12-28 23:08:41 +0200

    Add comments

--- src/index.js
+++ src/index.js
@@ -10,28 +10,35 @@ let menuBtn = document.getElementById('menu-btn'),
     menu = document.getElementById('menu'),
     menuItems = document.getElementsByClassName('nav-item'),
     contentContainer = document.getElementById('content');
-
+//for the first load
 showContent(data.projects[0].name);
 
+//menu animation
 menuBtn.addEventListener("click", toggleMenu.bind(null, menu, 'menu-active'));
+
+//menu items animation
 menu.addEventListener("click", clickItem, false);
 menu.addEventListener("mouseenter", highlightItem, true);
 menu.addEventListener("mouseleave", blurItem, true);
 
+//changing open/close menu icons
 function toggleMenu(menu, classname) {
     menuBtn.getElementsByTagName('i').item(0).classList.toggle('fa-bars');
     menuBtn.getElementsByTagName('i').item(0).classList.toggle('fa-times');
     menu.classList.toggle(classname);
 }
 function clickItem(e) {
+    //apply animation only to menu items
     if(e.target.classList.contains('nav-item')){
         Array.prototype.forEach.call(menuItems, i => {
+            //if there is already selected item - remove classes from it
                 if(i.classList.contains('nav-item-clicked')){
                     i.classList.remove('nav-item-clicked', 'nav-item-active');
                 }
             }
         );
         e.target.classList.add('nav-item-clicked', 'nav-item-active');
+        //if the same item selected - nothing changes
         if(document.getElementById('content-name').textContent!==e.target.textContent) {
             removeContent();
             showContent(e.target.textContent);
@@ -52,18 +59,23 @@ function blurItem(e) {
         }
     }
 }
-/*Dynamic content*/
+//Dynamic content, obtained from json by menu item name
 function showContent(name) {
     let content = data.projects.filter(i => {
         return i.name === name;
     })[0];
     console.log(content);
+
     let fragment = document.createDocumentFragment();
-    let nameDiv = document.createElement('div');
+
+    let nameDiv = document.createElement('div');//project name
     nameDiv.textContent = content.name;
     nameDiv.setAttribute('id', 'content-name');
     nameDiv.classList.add('content-name');
+
     fragment.appendChild(nameDiv);
+
+    //list of tasks
     for(let i = 0; i < content.tasks.length; i++){
         let task = document.createElement('div');
         task.textContent = content.tasks[i];
@@ -73,6 +85,7 @@ function showContent(name) {
         task.insertAdjacentElement('afterbegin', checkbox);
         fragment.appendChild(task);
     }
+
     contentContainer.appendChild(fragment);
 }
 function removeContent() {
