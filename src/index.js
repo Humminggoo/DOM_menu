05c48b263ede2be38b5bf46ccbc034fd41426426
Humminggoo <sashap1997@gmail.com>
2018-12-28 18:25:41 +0200

    Add menu animation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import  './css-reset.css'
 import './header.css';
 import  './menu.css'
+
+let menuBtn = document.getElementById('menu-btn'),
+    menu = document.getElementById('menu');
+
+menuBtn.addEventListener("click", toggleMenu.bind(null, menu, 'menu-active'));
+
+function toggleMenu(menu, classname) {
+    menu.classList.toggle(classname);
+}
