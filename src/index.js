4cf10a1fb916171bf027cac5febb740c08b3fbfd
Humminggoo <sashap1997@gmail.com>
2018-12-28 19:41:54 +0200

    Add menu items click and hover animation

--- src/index.js
+++ src/index.js
@@ -3,10 +3,39 @@ import './header.css';
 import  './menu.css'
 
 let menuBtn = document.getElementById('menu-btn'),
-    menu = document.getElementById('menu');
+    menu = document.getElementById('menu'),
+    menuItems = document.getElementsByClassName('nav-item');
 
 menuBtn.addEventListener("click", toggleMenu.bind(null, menu, 'menu-active'));
+menu.addEventListener("click", clickItem, false);
+menu.addEventListener("mouseenter", highlightItem, true);
+menu.addEventListener("mouseleave", blurItem, true);
 
 function toggleMenu(menu, classname) {
     menu.classList.toggle(classname);
+}
+function clickItem(e) {
+    if(e.target.classList.contains('nav-item')){
+        Array.prototype.forEach.call(menuItems, i => {
+                if(i.classList.contains('nav-item-clicked')){
+                    i.classList.remove('nav-item-clicked', 'nav-item-active');
+                }
+            }
+        );
+        e.target.classList.add('nav-item-clicked', 'nav-item-active');
+    }
+}
+function highlightItem(e) {
+    if(e.target.classList.contains('nav-item')) {
+        if (!e.target.classList.contains('nav-item-clicked')) {
+            e.target.classList.add('nav-item-active');
+        }
+    }
+}
+function blurItem(e) {
+    if(e.target.classList.contains('nav-item')) {
+        if (!e.target.classList.contains('nav-item-clicked')) {
+            e.target.classList.remove('nav-item-active');
+        }
+    }
 }
